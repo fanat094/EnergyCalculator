81eb24b55755b2e26d92d3cdbc86bed7e786c261
fanat94 <yamschikovdima@gmail.com>
2019-04-09 17:59:30 +0300

    Commit Permissions Logic

--- app/src/main/java/com/android/yamschikovdima/energycalculator/selectenergystate/presentation/view/SelectEnergyStateActivity.kt
+++ app/src/main/java/com/android/yamschikovdima/energycalculator/selectenergystate/presentation/view/SelectEnergyStateActivity.kt
@@ -6,12 +6,19 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Geocoder
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.Settings
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
@@ -31,6 +38,7 @@ import com.android.yamschikovdima.energycalculator.selectenergystate.presentatio
 import com.android.yamschikovdima.energycalculator.selectenergystate.presentation.viewmodel.SelectEnergyStateViewModel
 import com.android.yamschikovdima.energycalculator.utils.PermissionHelper
 import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
 import com.socks.library.KLog
 import javax.inject.Inject
@@ -60,14 +68,10 @@ class SelectEnergyStateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.select_energy_state_activity)
-
         component.inject(this)
         val binding = DataBindingUtil.setContentView<SelectEnergyStateActivityBinding>(
             this,
-            com.android.yamschikovdima.energycalculator.R.layout.select_energy_state_activity
-        )
-
-        //setEnergyStateList()
+            com.android.yamschikovdima.energycalculator.R.layout.select_energy_state_activity)
 
         binding.apply {
             lifecycleOwner.let {
@@ -80,114 +84,207 @@ class SelectEnergyStateActivity : AppCompatActivity() {
                 this@SelectEnergyStateActivity
             }, Observer {
 
-                //fabSearchEnergyStateDialog()
-                //getLocationFused()
-                setupPermissions()
-            })
+                if (checkNetConnection()) {
 
+                if (hasPermissions()) {
+                    getLocationFused()
+
+                } else {
+                    requestPermissionWithRationale()
+                }
+            }
+                else{
+
+                    tryAgainCheckNetConnection()
+                }
+            })
         }
 
         router.bindViewModel(viewModel)
     }
 
-    /*fun fabSearchEnergyState() {
+    // --- Permissions ---
+    private fun hasPermissions(): Boolean {
 
-//        KLog.e("fabSearchEnergyState", "Click")
-//        val intent = Intent(this, MainActivity::class.java)
-//        startActivity(intent)
-//        finish()
+        val permissions = arrayOf(
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
 
-        val file_name = "energy_state.json"
-        val json_string = application.assets.open(file_name).bufferedReader().use {
-            it.readText()
+        for (perms in permissions) {
+            val res = checkCallingOrSelfPermission(perms)
+            if (!(res == PackageManager.PERMISSION_GRANTED)) {
+                return false
+            }
         }
-        KLog.e("json_string", "read" + json_string)
-
-        val gson = Gson()
-        val listType = object : TypeToken<List<EnergyState>>() {}.type
-        val newList = gson.fromJson<List<EnergyState>>(json_string, listType)
-
-        KLog.json("responseObject", "------->" + newList)
+        return true
     }
 
-    private fun setEnergyStateList() {
+    private fun requestPerms() {
+        KLog.e("hasPermissions2_2_2", "hasPermissions2_2_2")
 
-        /*val file_name = "energy_state.json"
-        val json_string = application.assets.open(file_name).bufferedReader().use { it.readText() }
-        KLog.e("json_string", "read" + json_string)
+        val permissions = arrayOf(
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, PERMISSIONS_REQUEST_CODE)
+        }
+    }
 
-        val gson = Gson()
-        val listType = object : TypeToken<List<EnergyState>>() {}.type
-        val energyStateList = gson.fromJson<List<EnergyState>>(json_string, listType)
+    private fun requestPermissionWithRationale() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)
+            || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ) {
+            val message = "Storage permission is needed to show files count"
+            Snackbar.make(this.findViewById(R.id.appbar), message, Snackbar.LENGTH_LONG)
+                .setAction("GRANT", View.OnClickListener {
+                    requestPerms()
 
-        KLog.json("responseObject", "------->" + energyStateList)*/
-        //viewModel.setEnergyStateList(energyStateList)
-    }*/
+                }).show()
+        } else {
+            requestPerms()
+        }
+    }
 
-     fun fabSearchEnergyStateDialog() {
+    // --- Permissions Result ---
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+
+        var allowed = true
+
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE -> {
+
+                for (res in grantResults) {
+                    allowed = allowed && (res == PackageManager.PERMISSION_GRANTED)
+                }
+
+                if (allowed) {
+                    getLocationFused()
+                } else {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                            Toast.makeText(this, "Storage Permissions denied.", Toast.LENGTH_SHORT).show()
+
+                        } else {
+                            showNoStoragePermissionSnackbar()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // --- Permissions Settings ---
+    private fun showNoStoragePermissionSnackbar() {
+        Snackbar.make(this.findViewById(R.id.appbar), "Storage permission isn't granted", Snackbar.LENGTH_LONG)
+            .setAction("SETTINGS", View.OnClickListener {
+
+                openApplicationSettings()
+
+                Toast.makeText(
+                    applicationContext,
+                    "Open Permissions and grant the Storage permission",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }).show()
+    }
+
+    private fun openApplicationSettings() {
+        val appSettingsIntent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.parse("package:" + packageName)
+        )
+        startActivityForResult(appSettingsIntent, PERMISSIONS_REQUEST_CODE)
+    }
+
+    // --- Location Logic ---
+    private fun getLocationFused() {
+
+        KLog.e("fusedTag", "empty")
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            // place all code to get the last know location here along with onSuccessListener code
+            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener {
+                    if (it != null) {
+                        val geocoder = Geocoder(this)
+                        val address = geocoder?.getFromLocation(it.latitude, it.longitude, 1)
+                        KLog.e("regionLog", address[0].adminArea)
+                        Toast.makeText(
+                            applicationContext,
+                            "Region: " + address[0].adminArea,
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        fabSearchEnergyStateDialog(address[0].adminArea)
+                    }
+                }
+
+        } else {
+
+            //request permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPerms()
+            }
+        }
+    }
+
+
+    // --- Internet Utils ---
+    private fun checkNetConnection(): Boolean {
+
+        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+        return isConnected
+    }
+
+    private fun tryAgainCheckNetConnection(){
+
+        val message = "No Internet Connection!"
+        Snackbar.make(this.findViewById(R.id.appbar), message, Snackbar.LENGTH_LONG).show()
+    }
+
+    // --- Dialog Utils ---
+    private fun fabSearchEnergyStateDialog(regionStr:String) {
 
         this.let {
             MaterialDialog(it).show {
                 title(R.string.title_select_energy_state_cancel_selected)
-                message(R.string.title_select_energy_state_message)
-                positiveButton(R.string.title_select_energy_state_ok)
+                message(text = regionStr)
+                positiveButton(R.string.title_select_energy_state_ok) { _ ->
+                    KLog.e("Tagg","On positive")
+                    toMainActivity()
+                }
                 negativeButton(R.string.title_select_energy_state_cancel)
                 debugMode(debugMode)
             }
         }
     }
 
-    @SuppressLint("MissingPermission")
-    fun getLocationFused() {
-                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-                fusedLocationClient.lastLocation
-                    .addOnSuccessListener {
-                        if (it != null) {
-                            val geocoder = Geocoder(this)
-                            val address = geocoder?.getFromLocation(it.latitude, it.longitude, 1)
-                            KLog.e("fused",address[0].adminArea)
-                        }
-                    }
-
-    }
-
-    private fun setupPermissions() {
-        val permission = ContextCompat.checkSelfPermission(this,
-            Manifest.permission.RECORD_AUDIO)
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "Permission to record denied")
-            makeRequest()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            getLocationFused()
         }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
-    private fun makeRequest() {
-        ActivityCompat.requestPermissions(this,
-            arrayOf(Manifest.permission.INTERNET),
-            REQUEST_PERMISSIONS_REQUEST_CODE)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-
-        when (requestCode) {
-            REQUEST_PERMISSIONS_REQUEST_CODE -> {
-
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-
-                    Log.i(TAG, "Permission has been denied by user")
-                } else {
-                    Log.i(TAG, "Permission has been granted by user")
-                    getLocationFused()
-
-                }
-            }
-        }
+    private fun toMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     companion object {
-
-        private val TAG = "LocationProvider"
-
-        private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+        private val PERMISSIONS_REQUEST_CODE = 34
     }
 }
